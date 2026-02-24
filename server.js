14dd1bebcf00685ea836a0fa365c918bebd8e0ba
CamFinch <122115663+CamFinch@users.noreply.github.com>
2026-02-24 14:41:00 -0700

    commuted

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get("/calculate", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
